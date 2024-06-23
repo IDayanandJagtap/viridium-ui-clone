3ac99819207f885f9cc832653f2fc9dff39b116f
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2024-06-23 23:05:55 +0530

    fix background image path

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
                 primary: "#1C59FF",
             },
             backgroundImage: {
-                blueGrid: "url('./public/assets/blue-grid.png')",
+                blueGrid: "url('/assets/blue-grid.png')",
             },
             boxShadow: {
                 pillShadow: "-1px -1px 1px #1C59FF",
